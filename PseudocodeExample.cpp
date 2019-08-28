55458d9e4c2404646920f4d2807ed308560186cb
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-08-28 08:13:53 -0600

    add file for example psuedocode

---
+++ PseudocodeExample.cpp
@@ -0,0 +1,16 @@
+// Pseudocode Example 
+// Jacob Hardman
+// 8/28/19
+// Professer Bailey
+
+/*
+	Step 1: Get the price of a single apple for the day.
+	Step 2: Display the price of a single apple to the customer.
+	Step 3: Allow the user to input how many apples they would like to purchase.
+	Step 4: Get the number of apples to be purchased from the users input.
+	Step 5: Multiply the price of a single apple by the number of apples that the customer is buying.
+	Step 6: Display the total for the transaction to the customer.
+	Step 7: Have the user confirm that the total is correct.
+	Step 8: If the user confirms the total, process the transaction. Else, go back to step 2.
+	Step 9: Wish the user a good day.
+*/
